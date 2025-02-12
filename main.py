926262b039f2b6a5ff8f3e27b3a547242391fb9b
Konrad Thorsen <konrad.v.thorsen@gmail.com>
2025-02-12 15:18:17 +0100

    Venv

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import sys
+import utils
+import numpy as np
 
 
 if __name__ == "__main__":
     print("Which environment are we in?:", sys.prefix)
     print("hey!")
+
+    # readFile("ord.txt")
+    print(np.pi)
 
 
 
